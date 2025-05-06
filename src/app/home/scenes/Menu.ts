efdada7eef57f747583cb16c1878d1c90c5cfbd0
Sammy <svmmydev@gmail.com>
2025-05-07 00:00:15 +0200

    Etapa 1: Create dinamic background for all scenes (encapsulated)

--- src/app/home/scenes/Menu.ts
+++ src/app/home/scenes/Menu.ts
@@ -1,19 +1,22 @@
 import * as Phaser from 'phaser';
+import { createBackground, scrollBackground } from 'src/app/utils/createBackground';
 
 export class Menu extends Phaser.Scene {
+    private background!: Phaser.GameObjects.TileSprite;
+
     constructor() {
         super('menu');
     }
 
     preload() {
-
+        this.load.image('background', 'assets/imgs/background/backgroundseamless.png')
     }
 
     create() {
-
+        this.background = createBackground(this);
     }
 
     override update() {
-        
+        scrollBackground(this.background, 1);
     }
 }
--- src/app/home/scenes/Score.ts
+++ src/app/home/scenes/Score.ts
@@ -1,19 +1,22 @@
 import * as Phaser from 'phaser';
+import { createBackground, scrollBackground } from 'src/app/utils/createBackground';
 
 export class Score extends Phaser.Scene {
+    private background!: Phaser.GameObjects.TileSprite;
+
     constructor() {
         super('score');
     }
 
     preload() {
-        
+        this.load.image('background', 'assets/imgs/background/backgroundseamless.png')
     }
 
     create() {
-
+        this.background = createBackground(this);
     }
 
     override update() {
-        
+        scrollBackground(this.background, 1);
     }
 }
