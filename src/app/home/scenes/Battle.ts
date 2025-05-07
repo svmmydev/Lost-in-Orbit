863b14d7a00372237c9488be5bf8731b994e3012
Sammy <svmmydev@gmail.com>
2025-05-07 02:24:13 +0200

    Etapa 2: Implement menu screen

--- src/app/home/scenes/Battle.ts
+++ src/app/home/scenes/Battle.ts
@@ -1,19 +1,28 @@
 import * as Phaser from 'phaser';
-import { createBackground, scrollBackground } from 'src/app/utils/createBackground';
+import { createBackground, scrollBackground } from 'src/app/utils/manageBackground';
 
 export class Battle extends Phaser.Scene {
     private background!: Phaser.GameObjects.TileSprite;
+    private playerName: string = 'Anónimo';
 
     constructor() {
         super('battle');
     }
 
+    init(data: any) {
+        this.playerName = data.playerName ?? 'Anónimo';
+    }
+
     preload() {
-        this.load.image('background', 'assets/imgs/background/backgroundseamless.png')
+        
     }
 
     create() {
         this.background = createBackground(this);
+        this.add.text(this.scale.width / 2, this.scale.height / 2, `Bienvenido, ${this.playerName}`, {
+            fontSize: '28px',
+            color: '#ffffff',
+        }).setOrigin(0.5);
     }
 
     override update() {
--- src/app/home/scenes/Menu.ts
+++ src/app/home/scenes/Menu.ts
@@ -1,8 +1,10 @@
 import * as Phaser from 'phaser';
-import { createBackground, scrollBackground } from 'src/app/utils/createBackground';
+import { createBackground, scrollBackground } from 'src/app/utils/manageBackground';
 
 export class Menu extends Phaser.Scene {
     private background!: Phaser.GameObjects.TileSprite;
+    private inputElement!: Phaser.GameObjects.DOMElement;
+    private playerName: string = '';
 
     constructor() {
         super('menu');
@@ -10,13 +12,38 @@ export class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/imgs/background/backgroundseamless.png')
+        this.load.html('userForm', 'assets/html/userForm.html')
     }
 
     create() {
         this.background = createBackground(this);
+      
+        this.inputElement = this.add.dom(this.scale.width / 2, this.scale.height / 2).createFromCache('userForm');
+      
+        this.inputElement.addListener('click');
+        this.inputElement.on('click', (event: any) => {
+          if (event.target.name === 'startButton') {
+            this.procesarEntrada();
+          }
+        });
+      
+        this.input.keyboard?.on('keydown-ENTER', () => {
+          this.procesarEntrada();
+        });
     }
 
     override update() {
         scrollBackground(this.background, 1);
+    }
+      
+    private procesarEntrada() {
+      const input = this.inputElement.getChildByName('playerName') as HTMLInputElement;
+    
+      if (input?.value.trim()) {
+        this.playerName = input.value.trim();
+        this.scene.start('battle', { playerName: this.playerName });
+      } else {
+        alert('Introduce tu nombre');
+      }
     }
 }
